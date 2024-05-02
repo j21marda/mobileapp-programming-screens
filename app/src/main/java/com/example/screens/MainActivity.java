fa9ac7b6a3f634bd451213c7c2a5335d2caf675b
Polokurse <j21marda@student.his.se>
2024-05-02 21:46:07 +0200

    Skickar extra data i intent från main activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -17,7 +17,10 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.knapp);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", "j21marda");
+                intent.putExtra("number", 21);
+                startActivity(intent);
             }
         });
 
