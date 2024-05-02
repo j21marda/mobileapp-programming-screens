d8035a27f3874910ddf863010ad6b6e4496d115a
Polokurse <j21marda@student.his.se>
2024-05-02 21:52:09 +0200

    kod för att hämta datan från bundle extras

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -10,5 +10,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            int number = extras.getInt("number");
+            // Do something with the name and number
+        }
     }
 }
